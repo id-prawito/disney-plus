d2caaa3ca69b3a162425c87f5d8a4a40c4cde1cc
Prawito <prawito.id@gmail.com>
2021-12-22 05:58:51 +0700

    structure-folder

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,9 @@ import "./app.scss";
 function App() {
     return (
         <div className="App">
-            <h3 style={{ textAlign: "center" }}>Hello World 12 Tambah Bisa</h3>
+            <h3 style={{ textAlign: "center" }}>
+                Hello World 12 Tambah Bisa yok
+            </h3>
         </div>
     );
 }
