dfc9a2ef932c3512d51f41c35bbe0f4cc439ea0b
Prawito <prawito.id@gmail.com>
2021-12-22 03:12:12 +0700

    structure-folder

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import "./app.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <h3 style={{ textAlign: "center" }}>Hello World 12</h3>
+        </div>
+    );
+}
+
+export default App;
