a001690f770fc27ce464d1ea1a7daadf642ad08b
Prawito <prawito.id@gmail.com>
2021-12-22 06:01:44 +0700

    structure-folder

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,28 @@
 import "./app.scss";
+import "swiper/swiper.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
+// import Routes from "../config/RoutesDisney";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { GlobalProvider } from "../config/GlobalState";
+// import Scroll from "../components/Header/Scroll";
 
 function App() {
     return (
-        <div className="App">
-            <h3 style={{ textAlign: "center" }}>
-                Hello World 12 Tambah Bisa yok
-            </h3>
-        </div>
+        <GlobalProvider>
+            <BrowserRouter>
+                <Route
+                    render={(props) => (
+                        <>
+                            <Header {...props} />
+                            {/* <Scroll /> */}
+                            {/* <Routes /> */}
+                            <Footer />
+                        </>
+                    )}
+                />
+            </BrowserRouter>
+        </GlobalProvider>
     );
 }
 
