960323f743bf72bd17f230fbe40871c60dcbcff5
Prawito <prawito.id@gmail.com>
2021-12-27 15:09:27 +0700

    fix-search

--- src/components/Header/search.js
+++ src/components/Header/search.js
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { useState } from "react/cjs/react.development";
 import tmdbApi from "../../services/tmdbApi";
 import { MovieCardSearch, TvCardSearch } from "../MovieCard";
 import {
